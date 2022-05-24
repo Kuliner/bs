5df5fff6950c40c996fd00d132108f5e10eb9a83
Rafał Dunaj <duni154@gmail.com>
2022-05-24 22:00:50 +0200

    Update install_clamav.sh

--- install_clamav.sh
+++ install_clamav.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sudo apt-get update -y
-sudo apt-get install clamav clamav-daemon -y
-cp clamd.conf /etc/clamav/clamd.conf
-cp freshclam.conf /etc/clamav/freshclam.conf
-sudo service clamav-freshclam restart
+sudo apt-get update -y &&
+sudo apt-get install clamav clamav-daemon -y &&
+cp clamd.conf /etc/clamav/clamd.conf &&
+cp freshclam.conf /etc/clamav/freshclam.conf &&
+sudo service clamav-freshclam restart &&
 sudo service clamav-daemon start
