ee5fc2973417ab30c7c8d52d637ea2b9420929a8
Rafał Dunaj <duni154@gmail.com>
2022-05-24 23:54:47 +0200

    Update install_clamav.sh

--- install_clamav.sh
+++ install_clamav.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get install clamav-base clamav-daemon clamav-freshclam clamdscan clamassassin &&
+sudo apt-get install clamav-base clamav-daemon clamav-freshclam &&
 sed -i.old '1s;^;TCPSocket 3310\nTCPAddr 127.0.0.1\n;' /etc/clamav/clamd.conf &&
 echo "ListenStream=127.0.0.1:3310" | sudo tee -a /etc/systemd/system/clamav-daemon.service.d/extend.conf &&
 systemctl daemon-reload &&
