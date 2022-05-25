7010dd9086487d296257b6c17d48e928868d03c5
Rafał Dunaj <duni154@gmail.com>
2022-05-25 17:49:39 +0200

    Update install_clamav.ps1

--- install_clamav.ps1
+++ install_clamav.ps1
@@ -1,11 +1,18 @@
 Invoke-WebRequest -Uri https://www.clamav.net/downloads/production/clamav-0.105.0.win.x64.msi -OutFile c:\clamav-win-x64.msi -UseBasicParsing;
 Start-Process -FilePath "c:\clamav-win-x64.msi" -ArgumentList "/qn", "/l*vs", "c:\install.log" -Wait;
-
 Copy-Item "clamd.conf" "C:\Program Files\ClamAV\clamd.conf"; 
 Copy-Item "freshclam.conf" "C:\Program Files\ClamAV\freshclam.conf";
 
-New-Item -Path "C:\logs" -ItemType "directory"
-New-Item -Path "C:\db" -ItemType "directory"
+New-Item -Path "C:\logs" -ItemType "directory" -Force;
+New-Item -Path "C:\db" -ItemType "directory" -Force;
 
+Set-Location "C:\Program Files\ClamAV";
+
+Start-Process -FilePath "C:\Program Files\ClamAV\freshclam";
+Start-Sleep -Seconds 20;
+
+Start-Process -FilePath "C:\Program Files\ClamAV\freshclam" -ArgumentList "--install-service" -Wait;
 Start-Process -FilePath "C:\Program Files\ClamAV\clamd" -ArgumentList "--install-service" -Wait;
-Start-Process -FilePath "C:\Program Files\ClamAV\freshclam" -ArgumentList "--install-service" -Wait
+
+net start clamd;
+net start freshclam
